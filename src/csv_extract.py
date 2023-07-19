a248c4f3f573489530b82b664cc953c09035d098
Adrien Touzouli <contact@ad2ien.dev>
2023-07-19 15:21:34 +0200

    ✨ read csv file

---
+++ src/csv_extract.py
@@ -0,0 +1,22 @@
+import logging
+import csv
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+
+def read_file(csv_file):
+    logging.info("Read file : " + csv_file + "...")
+
+    line_count = 0
+    all_entries = []
+    with open(csv_file, mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            line_count += 1
+            if line_count == 0:
+                logging.info('read header...')
+                continue
+            all_entries += [row]
+
+    logging.info(str(len(all_entries)) + " lines")
+    return all_entries
--- src/runner.py
+++ src/runner.py
@@ -1,5 +1,10 @@
 import logging
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+from csv_extract import read_file
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
 def do_import_users(csv_file):
-    logging.info("Importing users from" + csv_file + "...")
+    logging.info("Importing users from " + csv_file + "...")
+
+    entries = read_file(csv_file)
